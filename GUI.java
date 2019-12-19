a79f486dba8ffc237481af90982b8bed6fb2dc16
Tueurdepizza <58038695+Tueurdepizza@users.noreply.github.com>
2019-12-19 16:16:34 +0100

    Add files via upload
    Avec JTextField pour entrer le lien

--- GUI.java
+++ GUI.java
@@ -13,17 +13,18 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-//import javax.swing.JTextField;
+import javax.swing.JTextField;
 
 
 public class GUI extends JFrame {
-	private JLabel valueLabel = new JLabel("Veuillez entrer un lien dans la console et appuyer sur un des boutons pour effecter l'action associée : ");
-	//private JTextField valueField = new JTextField(5);
+	private JLabel valueLabel = new JLabel("Veuillez entrer un lien dans la console et appuyer sur l'un des boutons pour effectuer l'action associée.");
+	private JTextField linkField = new JTextField(5);
 	private JButton arboParcours = new JButton("Parcours d'arborescence");
 	private JButton scan = new JButton("Scanner");
 	private JButton save = new JButton("Sauvegarder");
 	private JButton quitButton = new JButton("Quitter");
 	private JPanel rightPanel = new JPanel();
+	
 	/**
 	 * This is the model in MVC (model-view-controller).
 	 */
@@ -38,24 +39,24 @@ public class GUI extends JFrame {
 		save.addActionListener(new SaveAction());
 		quitButton.addActionListener(new QuitAction(this)); 
 		
-	
-		
 	}
 
 	private void init() {
 		BorderLayout border = new BorderLayout();
 		Container contentPane = getContentPane();
 		contentPane.setLayout(border);
-
-		contentPane.add(BorderLayout.WEST, valueLabel);
-		//contentPane.add(BorderLayout.CENTER, valueField);
-		contentPane.add(BorderLayout.SOUTH, quitButton);
-
+		
 		rightPanel.setLayout(new GridLayout(3, 1));
 		rightPanel.add(arboParcours);
 		rightPanel.add(scan);
 		rightPanel.add(save);
 		contentPane.add(BorderLayout.EAST, rightPanel);
+		
+		contentPane.add(BorderLayout.NORTH, valueLabel);
+		contentPane.add(BorderLayout.CENTER, linkField);
+		contentPane.add(BorderLayout.SOUTH, quitButton);
+		
+		
 
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		pack();
@@ -71,8 +72,8 @@ public class GUI extends JFrame {
 		public void actionPerformed(ActionEvent e) {
 		
 		System.out.println("Saississez le lien");
-		this.lien = sc.nextLine();
-		FilesWalk f = new FilesWalk(lien);		
+		lien = linkField.getText();
+		FilesWalk f = new FilesWalk(lien);	
 		}
 	}
 
@@ -83,7 +84,7 @@ public class GUI extends JFrame {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			System.out.println("Saississez le lien");
- 			lien = sc.nextLine();
+ 			lien = linkField.getText();
  			Informations i = new Informations(lien);
  			System.out.println(i);
 				Database d = new Database(i.getFileExt());
@@ -103,7 +104,7 @@ public class GUI extends JFrame {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			System.out.println("Saississez le lien");
- 			lien = sc.nextLine();
+ 			lien = linkField.getText();;
  			Informations i = new Informations(lien);
  			System.out.println(i);
 				Database d = new Database(i.getFileExt());
